f2471a6c74fc1320897da2d92bdd92f376cd4c63
Meg Ashby <megashby@comcast.net>
2026-05-03 15:43:03 -0400

    make ecs infra

---
+++ infra/ecs.tf
@@ -0,0 +1,124 @@
+module "ecs_cluster" {
+  source  = "terraform-aws-modules/ecs/aws"
+  version = "~> 6.0"
+
+  cluster_name = "${local.name}-cluster"
+}
+
+module "ecs_execution_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role"
+  version = "~> 6.0"
+
+  name = "${local.name}-ecs-exec-role"
+
+  create = true
+
+  trust_policy_permissions = {
+    TrustRoleAndServiceToAssume = {
+      actions = [
+        "sts:AssumeRole",
+      ]
+      principals = [{
+        type = "Service"
+        identifiers = [
+          "ecs-tasks.amazonaws.com",
+        ]
+      }]
+    }
+  }
+
+  policies = {"TaskExecution" = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"}
+
+}
+
+module "ecs_task_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role"
+  version = "~> 6.0"
+
+  name = "${local.name}-ecs-task-role"
+
+  create = true
+
+  trust_policy_permissions = {
+    TrustRoleAndServiceToAssume = {
+      actions = [
+        "sts:AssumeRole",
+      ]
+      principals = [{
+        type = "Service"
+        identifiers = [
+          "ecs-tasks.amazonaws.com",
+        ]
+      }]
+    }
+  }
+
+  policies = {"TaskExecution" = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"}
+
+  inline_policy_permissions = {
+    VpcLatticeInvoke = {
+      effect = "Allow"
+
+      actions = [
+        "vpc-lattice-svcs:Invoke"
+      ]
+
+      resources = ["*"]
+    }
+  }
+}
+
+resource "aws_ecs_task_definition" "client" {
+  family                   = "client"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
+  execution_role_arn       = module.ecs_execution_role.arn
+  task_role_arn            = module.ecs_task_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name  = "client"
+      image = "public.ecr.aws/docker/library/node:18"
+
+      command = ["node", "-e", <<EOF
+const https = require('https');
+setInterval(() => {
+  https.get('https://${aws_vpclattice_service.backend.dns_entry[0].domain_name}', res => {
+    console.log("client status:", res.statusCode);
+  }).on('error', e => console.error(e.message));
+}, 5000);
+EOF
+      ]
+
+      essential = true
+    }
+  ])
+}
+
+resource "aws_ecs_service" "client_a" {
+  name            = "client-a"
+  cluster         = module.ecs_cluster.cluster_id
+  task_definition = aws_ecs_task_definition.client.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = module.ecs_vpc.private_subnets
+    assign_public_ip = false
+  }
+}
+
+resource "aws_ecs_service" "client_b" {
+  name            = "client-b"
+  cluster         = module.ecs_cluster.cluster_id
+  task_definition = aws_ecs_task_definition.client.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = module.ecs_vpc.private_subnets
+    assign_public_ip = false
+  }
+}
--- infra/vpc-lattice.tf
+++ infra/vpc-lattice.tf
@@ -1,11 +1,13 @@
-locals {
-    name = "demo-lattice"
-}
-
 resource "aws_vpclattice_service_network" "this" {
   name = "${local.name}-network"
+}
+
+resource "aws_vpclattice_service_network_vpc_association" "ecs_vpc" {
+  vpc_identifier             = module.ecs_vpc.vpc_id
+  service_network_identifier = aws_vpclattice_service_network.this.id
 }
 
 resource "aws_vpclattice_service" "backend" {
   name = "${local.name}-backend-service"
 }
+
