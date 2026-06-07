bdc8f42817bb3687db020c0e9ae5db561256045c
Meg Ashby <megashby@comcast.net>
2026-06-06 20:40:35 -0400

    remove db and dbproxy stuff

--- infra/vpc-lattice.tf
+++ infra/vpc-lattice.tf
@@ -17,53 +17,6 @@ resource "aws_vpclattice_service_network_vpc_association" "service_vpc" {
   service_network_identifier = aws_vpclattice_service_network.this.id
 }
 
-resource "aws_vpclattice_service" "proxy" {
-  name = "${local.name}-db-proxy-service"
-}
-
-resource "aws_vpclattice_target_group" "proxy" {
-  name = "${local.name}-db-proxy-tg"
-  type = "IP"
-
-  config {
-    protocol = "HTTP"
-    port     = 3000
-
-    vpc_identifier = module.service_vpc.vpc_id
-
-    health_check {
-      enabled  = true
-      protocol = "HTTP"
-      path     = "/health"
-      port     = 3000
-
-      health_check_interval_seconds = 10
-
-      healthy_threshold_count   = 2
-      unhealthy_threshold_count = 2
-    }
-  }
-}
-
-resource "aws_vpclattice_listener" "proxy" {
-  name               = "http"
-  protocol           = "HTTP"
-  port               = 80
-  service_identifier = aws_vpclattice_service.proxy.id
-
-  default_action {
-    forward {
-      target_groups {
-        target_group_identifier = aws_vpclattice_target_group.proxy.id
-      }
-    }
-  }
-}
-
-resource "aws_vpclattice_service_network_service_association" "proxy" {
-  service_identifier         = aws_vpclattice_service.proxy.id
-  service_network_identifier = aws_vpclattice_service_network.this.id
-}
 
 resource "aws_cloudwatch_log_group" "lattice_access_logs" {
   name              = "/aws/vpclattice/${local.name}-access-logs"
@@ -94,4 +47,27 @@ resource "aws_cloudwatch_log_resource_policy" "vpclattice" {
       }
     ]
   })
+}
+
+module "db_proxy_ecs_infra_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role"
+  version = "~> 6.0"
+
+  name = "${local.name}-db-proxy-ecs-infra-role"
+
+  create = true
+
+  trust_policy_permissions = {
+    ecs = {
+      actions = ["sts:AssumeRole"]
+      principals = [{
+        type        = "Service"
+        identifiers = ["ecs.amazonaws.com"]
+      }]
+    }
+  }
+
+  policies = {
+    infra = "arn:aws:iam::aws:policy/AmazonECSInfrastructureRolePolicyForVpcLattice"
+  }
 }
